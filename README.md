1261334837d95c1cda36faad3bca3fe7dc6d5e6c
Dimitris-Provatas <james.provatas@gmail.com>
2020-08-11 20:34:36 +0300

    New loop for multiple inputs, added README.md
    Added a new loop to let the users input multiple strings without rerunning the program. Added a README file with examples for better understanding of the program's functionality.

---
+++ README.md
@@ -0,0 +1,12 @@
+A python program that gets a string as input, partitions it in letters that duplicate them selves, and prints an array with the partitioned sting.
+
+Run the following command in the downloaded folder:
+<code>python3 partition.py </code>
+
+E.G. for the workflow:
+
+**Input**: <code>atestingly</code>
+
+**Output**: <code>['a', 'test', 'i', 'n', 'g', 'l', 'y']</code>
+
+After that, the user is asked if he wants to enter a new string. Answering ['yes', 'y', 'YES', 'Y'] reruns the program.
--- partition.py
+++ partition.py
@@ -1,40 +1,52 @@
 import math
 
 def main():
-    r = input("Type in a string: ")
-    pos = []
-    sets = []
-    letterSets = []
+    while True:
+        r = input("Type in a string: ")
+        pos = []
+        sets = []
+        letterSets = []
 
-    # print(len(r))
-    for i in range(0, len(r)):
-        for j in  range(len(r) - 1, i - 1, -1):
-            # print(i, j)
-            if (i <= j):
-                if (r[i] == r[j]):
-                    pos.append([i, j])
-                    break
-    pos.append([math.inf, math.inf])
+        # print(len(r))
+        for i in range(0, len(r)):
+            for j in  range(len(r) - 1, i - 1, -1):
+                # print(i, j)
+                if (i <= j):
+                    if (r[i] == r[j]):
+                        pos.append([i, j])
+                        break
+        pos.append([math.inf, math.inf])
 
-    i = 0
-    myMin = 0
-    myMax = 0
+        i = 0
+        myMin = 0
+        myMax = pos[0][1]
 
-    while (i >= myMin) and (i < len(pos) - 1):
-        string = ""
-        if (myMax >= pos[i + 1][0]):
-            myMax = max(myMax, pos[i + 1][1])
-            i += 1
+        while (i >= myMin) and (i < len(pos) - 1):
+            string = ""
+            if (myMax >= pos[i + 1][0]):
+                myMax = max(myMax, pos[i + 1][1])
+                i += 1
+            else:
+                myMax = pos[i][1]
+                sets.append([myMin, myMax])
+                for j in range(myMin, myMax + 1):
+                    string += r[j]
+                myMin = myMax + 1
+                i = myMin
+                letterSets.append(string)
+
+        print("Partitioned string is: " + str(letterSets))
+
+        newInput = input("Input a new string? (Yes/No): ")
+
+        if (newInput == "yes" or newInput == "y" or newInput == "YES" or newInput == "Y"):
+            continue
+        elif (newInput == "no" or newInput == "n" or newInput == "NO" or newInput == "N"):
+            print("Exiting...")
+            exit()
         else:
-            myMax = pos[i][1]
-            sets.append([myMin, myMax])
-            for j in range(myMin, myMax + 1):
-                string += r[j]
-            myMin = myMax + 1
-            i = myMin
-            letterSets.append(string)
-
-    print("Partitioned string is: " + str(letterSets))
+            print("No valid answer, terminating!")
+            exit()
 
 if (__name__) == "__main__":
     main()
